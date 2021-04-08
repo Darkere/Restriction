6c306d14c18980b590fa03fb9276ff00732ef634
Darkere <Dark-277@web.de>
2021-04-08 22:38:51 +0200

    more WIP refactor

--- src/main/java/com/davqvist/restriction/RestrictionTypes/OrRestriction.java
+++ src/main/java/com/davqvist/restriction/RestrictionTypes/OrRestriction.java
@@ -5,23 +5,28 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class OrRestriction extends RestrictionType{
-    public OrRestriction(RestrictionReader.Descriptor descriptor) {
+public class OrRestriction extends RestrictionType {
+    public static String ID = "OR";
+    RestrictionType first;
+    RestrictionType second;
+    public OrRestriction(RestrictionReader.Descriptor descriptor, RestrictionType first, RestrictionType second) {
         super(descriptor);
+        this.first = first;
+        this.second = second;
     }
 
     @Override
     public boolean test(World world, BlockPos pos, PlayerEntity player) {
-        return super.test(world, pos, player);
+        return first.test(world, pos, player) || second.test(world, pos, player);
     }
 
     @Override
     public String getID() {
-        return super.getID();
+        return ID;
     }
 
     @Override
     public String getMessage() {
-        return super.getMessage();
+        return first.getMessage() + " or " + second.getMessage();
     }
 }
